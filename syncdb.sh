58faad2d3256fff6ba5085b79611ffebec66d7f5
Fernando Andrade <fernando.neto@gmail.com>
2014-06-03 12:28:02 -0300

    first

---
+++ syncdb.sh
@@ -0,0 +1,1 @@
+foreman run python manage.py syncdb
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/partec/Dev/heroku/bolao/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
