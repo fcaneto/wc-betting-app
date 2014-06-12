4921754df05bda0bfe5dc70622d89d742e9fffef
Fernando Andrade <fernando.neto@gmail.com>
2014-06-11 23:00:42 -0300

    non intrusive bet validation

--- bolao/management/commands/validate.py
+++ bolao/management/commands/validate.py
@@ -10,9 +10,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        max_score = 6 * 64 + 16*6 + 8*8 + 4*10 + 12 + 24
-        print 'MAX is %s' % max_score
-
         for bet_room in BetRoom.objects.all():
             print 'Processing ' + bet_room.name
 
@@ -30,41 +27,73 @@ class Command(BaseCommand):
             round_of_16 = []
             quarter = []
             semi = []
+            finals = []
+
+            round_of_16_winners = []
+            quarter_winners = []
+            semi_winners = []
 
             error = ""
             for i, player in enumerate(player_list):
                 if len(all_bets[player.id]) > 0:
                     for game_id in range(49, 57):
-                        home = all_bets[player.id][game_id].home_team
-                        away = all_bets[player.id][game_id].away_team
+                        bet = all_bets[player.id][game_id]
+                        home = bet.home_team
+                        away = bet.away_team
                         round_of_16.append(home)
                         round_of_16.append(away)
+                        round_of_16_winners.append(bet.get_winner())
                     for game_id in range(57, 61):
-                        home = all_bets[player.id][game_id].home_team
-                        away = all_bets[player.id][game_id].away_team
+                        bet = all_bets[player.id][game_id]
+                        home = bet.home_team
+                        away = bet.away_team
                         if home not in round_of_16 or away not in round_of_16:
                             error = 'Erro nas quartas - %s x %s' % (home.name, away.name)
                             break
                         quarter.append(home)
                         quarter.append(away)
+                        quarter_winners.append(bet.get_winner())
                     if not error:
                         for game_id in [61, 62]:
-                            home = all_bets[player.id][game_id].home_team
-                            away = all_bets[player.id][game_id].away_team
+                            bet = all_bets[player.id][game_id]
+                            home = bet.home_team
+                            away = bet.away_team
                             if home not in quarter or away not in quarter:
                                 error = 'Erro nas semis'
                                 break
                             semi.append(home)
                             semi.append(away)
+                            semi_winners.append(bet.get_winner())
                     if not error:
                         for game_id in [63, 64]:
-                            home = all_bets[player.id][game_id].home_team
-                            away = all_bets[player.id][game_id].away_team
+                            bet = all_bets[player.id][game_id]
+                            home = bet.home_team
+                            away = bet.away_team
+                            finals.append(home)
+                            finals.append(away)
                             if home not in quarter or away not in quarter:
                                 error = 'Erro nas finais'
                                 break
+
+                for w in round_of_16_winners:
+                    if w not in quarter:
+                        error = u'Oitavas - Vencedor não passou!'
+                        break
+
+                if not error:
+                    for w in quarter_winners:
+                        if w not in semi:
+                            error = u'Quartas - Vencedor não passou!'
+
+                if not error:
+                    for w in semi_winners:
+                        if w not in finals:
+                            error = u'Semi - Vencedor não passou!'
+
                 if error:
                     print 'Player %s: %s' % (player.user.first_name, error)
+                else:
+                    print 'Player %s: OK' % (player.user.first_name)
 
 
         print 'Done!'
