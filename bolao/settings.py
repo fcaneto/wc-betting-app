d8edcde81ba2203f72aa7c42baab8c6026c4f73a
Fernando Andrade <fernando.neto@gmail.com>
2014-06-25 16:57:31 -0300

    Connecting to memcache.

--- bolao/settings.py
+++ bolao/settings.py
@@ -90,3 +90,27 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+# Memcache
+
+def get_cache():
+  import os
+  try:
+    os.environ['MEMCACHE_SERVERS'] = os.environ['MEMCACHIER_SERVERS'].replace(',', ';')
+    os.environ['MEMCACHE_USERNAME'] = os.environ['MEMCACHIER_USERNAME']
+    os.environ['MEMCACHE_PASSWORD'] = os.environ['MEMCACHIER_PASSWORD']
+    return {
+      'default': {
+        'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+        'TIMEOUT': 500,
+        'BINARY': True,
+        'OPTIONS': { 'tcp_nodelay': True }
+      }
+    }
+  except:
+    return {
+      'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+      }
+    }
+
+CACHES = get_cache()
