b1792e78e7a8df71a105e1ad13ec205e38a51d1f
Fernando Andrade <fernando.neto@gmail.com>
2014-07-11 17:20:12 -0300

    debug toolbar

--- bolao/settings.py
+++ bolao/settings.py
@@ -85,7 +85,8 @@ TEMPLATE_DIRS = (
 
 # Debug Toolbar
 
-#DEBUG_TOOLBAR_PATCH_SETTINGS = False
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+INTERNAL_IPS = os.getenv('DEBUG_TOOLBAR_IP', '127.0.0.1')
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
--- bolao/urls.py
+++ bolao/urls.py
@@ -21,10 +21,10 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-#if settings.DEBUG:
-#    import debug_toolbar
-#    urlpatterns += patterns('',
-#        url(r'^__debug__/', include(debug_toolbar.urls)),
-#    )
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
 
 
