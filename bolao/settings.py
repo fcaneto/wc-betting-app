b59fd01a520ce7ca640932847899e8d9117413f9
Fernando Andrade <fernando.neto@gmail.com>
2014-07-11 17:38:41 -0300

    debug toolbar

--- bolao/settings.py
+++ bolao/settings.py
@@ -62,7 +62,7 @@ INSTALLED_APPS = (
     'bolao',
     'south',
     'gunicorn',
-    'debug_toolbar'
+    #'debug_toolbar'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -72,7 +72,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'bolao.urls'
